67c4cc3124ccdf3948fb76513a483849d2564372
fsoeltoni <fsoeltoni@gmail.com>
2019-12-25 13:52:35 +0700

    Configure react-admin

---
+++ src/providers/attrs.js
@@ -0,0 +1,4 @@
+export const title = "SIM TNI-AD";
+export const server_ip = "http://localhost";
+export const self_ip = "http://localhost";
+export const server_port = 3000;
---
+++ src/providers/data.js
@@ -0,0 +1,6 @@
+import jsonServerProvider from "ra-data-json-server";
+import { server_ip, server_port } from "./attrs";
+
+const mockDataProvider = jsonServerProvider(server_ip + ":" + server_port);
+
+export default { mockDataProvider };
