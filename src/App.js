f604c7acea95e154ec9e6a4ed56de85739089daf
fsoeltoni <fsoeltoni@gmail.com>
2019-12-25 15:01:36 +0700

    Create res lingkup

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Admin, Resource } from "react-admin";
 import { title } from "./providers/attrs";
 import data from "./providers/data";
 import kota_kab from "./resources/kota_kab";
+import lingkup from "./resources/lingkup";
 
 const dataProvider = data;
 
 const App = () => (
   <Admin title={title} dataProvider={dataProvider}>
+    <Resource {...lingkup.identities} />
     <Resource {...kota_kab.identities} />
   </Admin>
 );
